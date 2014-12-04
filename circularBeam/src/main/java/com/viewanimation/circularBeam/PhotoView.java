be2154f9f4c571a3c7606704e482b027d646cfce
AbrahamArun <arun4xp@gmail.com>
2014-12-04 15:52:05 +0530

    Test commit

--- circularBeam/src/main/java/com/viewanimation/circularBeam/PhotoView.java
+++ circularBeam/src/main/java/com/viewanimation/circularBeam/PhotoView.java
@@ -87,23 +87,17 @@ public class PhotoView extends View {
     private Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth) {
 
         int width = bm.getWidth();
-
         int height = bm.getHeight();
-
         float scaleWidth = ((float) newWidth) / width;
-
         float scaleHeight = ((float) newHeight) / height;
 
         // CREATE A MATRIX FOR THE MANIPULATION
-
         Matrix matrix = new Matrix();
 
         // RESIZE THE BIT MAP
-
         matrix.postScale(scaleWidth, scaleHeight);
 
         // RECREATE THE NEW BITMAP
-
         return Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
 
     }
